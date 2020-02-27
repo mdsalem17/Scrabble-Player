f3bfafcc6e2271fdf30c807ab5bf01a589178fe5
mdsalem17 <mmsalem78@gmail.com>
2020-02-27 20:27:53 +0100

    addPlus function, updating the lexicon class, testing features

---
+++ src/include.cpp
@@ -0,0 +1,34 @@
+#include "include.hpp"
+
+std::pair<std::string, std::string> splitString(std::string word, unsigned int position){
+    if(position > word.size()){
+        std::cout<<"Position too big"<<std::endl;
+        exit(EXIT_FAILURE);
+    }else {
+        std::string subWord1="";
+        std::string subWord2="";
+        for(unsigned int i=0; i<word.size(); i++){
+            if(i<=position){
+                subWord1+=word[i];
+            }else{
+                subWord2+=word[i];
+            }
+        }
+        return std::make_pair(subWord1, subWord2);
+    }
+}
+
+std::string reverseStr(std::string word){ 
+    unsigned int n = word.length();
+    std::string str = word;
+    for (unsigned int i = 0; i < n; i++) 
+        str[i] = word[n - i - 1];
+    return str;
+}
+
+void printArray(std::vector<std::string> input){
+	for (unsigned int i = 0; i < input.size(); i++) {
+		std::cout << input.at(i) <<" "<<std::endl;
+	}
+    std::cout<<std::endl;
+}
---
+++ src/include.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+    
+std::pair<std::string, std::string> splitString(std::string word, unsigned int position);
+std::string reverseStr(std::string str);
+void printArray(std::vector<std::string> input);
