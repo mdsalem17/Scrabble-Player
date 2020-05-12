74dff2cfbd25f2950db05105c21fae49a433dcae
ABDRABO <khaled.abdrabo@etu.univ-lyon1.fr>
2020-05-12 08:09:40 +0200

    last commit 🤞

--- src/bag.hpp
+++ src/bag.hpp
@@ -5,18 +5,18 @@ class Bag {
         Bag();
         ~Bag();
 
-        // "generateLetter" qui permet de piocher une letter du sac
+        // "generate_letter" qui permet de piocher une letter du sac
         // en temps constant
         char generate_letter();
         
-        // "displayBag" affiche le contenu du sac
+        // "display_bag" affiche le contenu du sac
         void display_bag();
         
-        // "getCharPoints" renvoie le score d'une lettre passée en paramètre
+        // "get_character_points" renvoie le score d'une lettre passée en paramètre
         // utilisée pour calculer le score final d'un mot
         unsigned int get_character_points(char _letter) const;
         
-        // "getNbLetters" renvoie le nombre de lettres restantes dans le sac
+        // "get_nbLetters" renvoie le nombre de lettres restantes dans le sac
         unsigned int get_nbLetters() const;
 
     private:
