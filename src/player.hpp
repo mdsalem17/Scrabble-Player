d6eeaa2fd9f3bbe3114f4c3a51c407edda3c9ce6
ABDRABO <khaled.abdrabo@etu.univ-lyon1.fr>
2020-05-12 08:08:04 +0200

    Update commentaires player.hpp

--- src/player.hpp
+++ src/player.hpp
@@ -10,17 +10,17 @@ class Player{
         Player();
         ~Player();
 
-        // "getNbHandLetters" renvoie le nombre de lettres dans la main
+        // "get_nbHandLetters" renvoie le nombre de lettres dans la main
         unsigned int get_nbHandLetters();
 
-        // "setNbHandLetters" utilisée pour définir le nombre de lettres de la main
+        // "set_nbHandLetters" utilisée pour définir le nombre de lettres de la main
         void set_nbHandLetters(unsigned int nb_letters);
 
-        // "replaceLetters" remplace les lettres utilisée pour construit le mot
+        // "replace_letters" remplace les lettres utilisée pour construit le mot
         // sur le plateau
         bool replace_letters(std::string word);
 
-        // "handToString" renvoie les lettres de la main sous forme d'une chaine
+        // "hand_to_string" renvoie les lettres de la main sous forme d'une chaine
         // de caractères (string)
         std::string hand_to_string();
 
